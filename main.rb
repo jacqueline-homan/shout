10fdef52a3ce2d6ea0511e29d736a2c3f05bcf65
Ed Drain <geekprogrammer.ed@gmail.com>
2013-12-17 15:23:23 -0500

    before_styles

--- main.rb
+++ main.rb
@@ -3,22 +3,25 @@ require 'sinatra/reloader' if development?
 require 'slim'
 require 'omniauth-twitter'
 require 'pry'
+require 'sass'
 
-configure :development do
-  use BetterErrors::Middleware 
-  BetterErrors.application_root = File.expand_path('..', __FILE__)
-  BetterErrors.editor = :sublime if defined? BetterErrors
+
+#use BetterErrors::Middleware   
+#BetterErrors.application_root = File.expand_path('..', __FILE__)
+#BetterErrors.editor = :sublime if defined? BetterErrors
 enable :sessions
 
 use OmniAuth::Builder do
 	provider :twitter, ENV['TFWS_KEY'], ENV['TFWS_SECRET']
 end 
-get '/style.css' { scss :styles }
+get '/style.css' do
+ 	scss :styles 
+end
 get '/' do
-	erb :home
+	slim :home
 end
 get '/about' do
-	erb :about 
+	slim :about 
 end
 get '/auth/twitter/callback' do
 	binding.pry
